c0cc922c2e21da9587b6069e640d44ee7d85419f
unknown <alazim-star>
2024-11-18 01:25:01 +0600

    add repo link

---
+++ FireBase/fireBase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1njGoF8UsOUDpxnY5cKM1MU55OcQMvbs",
+  authDomain: "b10-a9-authentication.firebaseapp.com",
+  projectId: "b10-a9-authentication",
+  storageBucket: "b10-a9-authentication.firebasestorage.app",
+  messagingSenderId: "746040934712",
+  appId: "1:746040934712:web:6c73a17f821000be70c1cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+github repo link: https://github.com/programming-hero-web-course1/b10-a9-authentication-alazim-star
+document link: https://docs.google.com/document/d/1uck_6gjiW22ytQMUEdznPxuxX4XB9wDz6fHJroTqCps/edit?usp=drive_link
